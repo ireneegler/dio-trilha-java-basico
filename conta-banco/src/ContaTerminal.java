7b396a865b28e69bd8642da21bd3d7afe414c317
Irene Egler Aguiar <irene.aguiar@dmz.org.br>
2023-10-20 22:49:50 -0300

    Desafio concluido

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.io.Console;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: meet and import the scanner class
+        //TODO: Display messages to our user
+        //TODO: Obtain the values ​​entered in the terminal via the scanner
+        //TODO: Display the message "Account Created"
+
+        Scanner terminal = new Scanner(System.in);
+
+        int numero = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "Mario Andrade";
+        double decimal = 237.48;
+
+        System.out.println("Digite o número da agência: " + agencia);
+        System.out.println("Usuário: " + numero);
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + decimal + " já está disponível para saque");
+        
     }
 }
